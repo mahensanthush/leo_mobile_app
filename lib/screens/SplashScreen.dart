2d0d0b1f60f33659f5f01ccf082e14d2dd737191
mahensanthush <mahensanthush4@gmail.com>
2024-01-03 02:23:15 +0530

    leo_mobile_app

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:leo_mobile_app/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _animationController;
+  Animation<double>? _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController!);
+    _animationController!.forward();
+    Timer(Duration(seconds: 3), () {
+      // Navigate to the home screen after 3 seconds
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: FadeTransition(
+        opacity: _animation!,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 10),
+              Container(
+                child: Text(
+                  "UOK LEO",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+              // CircularProgressIndicator(
+              //   valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+//
